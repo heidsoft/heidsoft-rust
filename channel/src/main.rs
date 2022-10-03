837f5a9dc2434670c482b765c899d5f9974df34c
jake.liu <jake.liu@baozun.com>
2022-10-03 13:44:00 +0800

    管道父子线程传递参数

--- channel/src/main.rs
+++ channel/src/main.rs
@@ -1,5 +1,16 @@
+use std::sync::mpsc;
+use std::thread;
+
 fn main() {
     println!("Hello, world!");
-    /// 创建了一个新通道
-    let (tx,rx) = mpsc::channel();
+    //创建了一个新通道
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+
+    // 主线程接受打印
+    let received = rx.recv().unwrap();
+    println!("Got: {}",received);
 }
