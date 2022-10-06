790cce92fb8feaeee2f30fa454486d81a1eee438
jake.liu <jake.liu@baozun.com>
2022-10-06 09:08:42 +0800

    add rust http web server

---
+++ httpserver/src/main.rs
@@ -0,0 +1,19 @@
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
+fn main() {
+    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let _stream = stream.unwrap();
+        println!("Connection established");
+        handle_connection(_stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0;512];
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}",String::from_utf8_lossy(&buffer[..]));
+}
